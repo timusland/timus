24150c15f954d66c2cd280ad7a507dfceef10c22
conradocarvalho <conrado@decarva.com.br>
2020-03-07 07:41:12 -0300

    test(parser): add acorn tests-regexp-2018

--- packages/parser/test/acorn-tests/index.js
+++ packages/parser/test/acorn-tests/index.js
@@ -5,6 +5,7 @@ function run(lang) {
     const testsDynamicImport = require('./tests-dynamic-import')
     const testsEs7 = require('./tests-es7')
     const testsOptionalCatchBinding = require('./tests-optional-catch-binding')
+    const testsRegexp2018 = require('./tests-regexp-2018')
 
     tests.run(lang)
     testsAwaitTopLevel.run(lang)
@@ -12,6 +13,7 @@ function run(lang) {
     testsDynamicImport.run(lang)
     testsEs7.run(lang)
     testsOptionalCatchBinding.run(lang)
+    testsRegexp2018.run(lang)
 }
 
 module.exports = { run }
